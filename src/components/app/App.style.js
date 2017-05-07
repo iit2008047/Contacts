cd54158bae3ec6674efea28f0504f952ddfcdbbc
faiz.karim <faiz.karim@sprinklr.com>
2017-05-07 06:37:45 +0530

    add routers

---
+++ src/components/app/App.style.js
@@ -0,0 +1,12 @@
+/**
+ * @author: faiz karim
+ * @since: 02/05/17
+ */
+
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  navigationBar: {
+    backgroundColor: '#F6F6F6',
+  },
+})
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,15 +6,36 @@
 import {Scene, Router} from 'react-native-router-flux';
 import React, { Component } from 'react';
 import ContactList from '../contactList'
+import DetailPane from '../detailPane'
+import EditContact from '../editContact'
+
+import Styles from './App.style'
 
 class App extends Component {
   render() {
-    return <Router>
+    return <Router getSceneStyle={getSceneStyle} navigationBarStyle={Styles.navigationBar}>
       <Scene key="root">
         <Scene key="ContactList" component={ContactList} title="Contact"/>
+        <Scene key="DetailPane" component={DetailPane} title=""/>
+        <Scene key="EditContact" component={EditContact} title=""/>
       </Scene>
     </Router>
   }
 }
+
+const getSceneStyle = (/* NavigationSceneRendererProps */ props, computedProps) => {
+  const style = {
+    flex: 1,
+    backgroundColor: '#F6F6F6',
+    shadowColor: null,
+    shadowOffset: null,
+    shadowOpacity: null,
+    shadowRadius: null,
+  };
+  if (computedProps.isActive) {
+    style.marginTop = computedProps.hideNavBar ? 0 : 64;
+  }
+  return style;
+};
 
 export default App;
